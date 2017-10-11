58c376d9814f5f6ce49f8ee45fb69540f9f0e603
Marcel Miranda <mmiranda@travix.com>
2017-10-11 15:09:57 +0200

    Pass each var value thru template function

--- src/processors/js.js
+++ src/processors/js.js
@@ -1,6 +1,19 @@
+const template = require('lodash.template');
+
+function parseExpressions(obj) {
+  return Object.keys(obj).reduce((result, key) => {
+    const value = obj[key];
+    return Object.assign(result, {
+      [key]: typeof value === 'string'
+        ? template(value)(obj)
+        : parseExpressions(value)
+    });
+  }, {});
+}
+
 const JsProcessor = {
   compile(obj) {
-    return obj;
+    return parseExpressions(obj);
   }
 };
 
