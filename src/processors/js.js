5a15054f85d04556ef7091e600f79da226667f7e
Marcel Miranda <mmiranda@travix.com>
2017-10-12 12:43:13 +0200

    Allow applying transformations on object trees

--- src/processors/js.js
+++ src/processors/js.js
@@ -1,14 +1,39 @@
 const template = require('lodash.template');
 
-function parseExpressions(obj) {
-  return Object.keys(obj).reduce((result, key) => {
+/**
+ * Simple check if passed argument is object or array
+ *
+ * @param {any}
+ */
+function isObject(obj) {
+  return obj === Object(obj);
+}
+
+/**
+ * Apply transform on primitive values (strings, numbers)
+ *
+ * @param {Object|Array} value
+ * @param {Object} local variables to pass into transformation
+ */
+function applyTransforms(value, obj) {
+  return template(value)(obj);
+}
+
+/**
+ * This function will walk a object tree and apply transformations on it
+ *
+ * @param {Object} obj
+ * @param {Object} the parent object
+ */
+function parseExpressions(obj, proto = null) {
+  const parsedObj = Object.create(proto || obj);
+  Object.keys(obj).forEach((key) => {
     const value = obj[key];
-    return Object.assign(result, {
-      [key]: typeof value === 'string'
-        ? template(value)(obj)
-        : parseExpressions(value)
-    });
-  }, {});
+    const fn = isObject(value) ? parseExpressions : applyTransforms;
+    parsedObj[key] = fn(value, parsedObj);
+  });
+
+  return parsedObj;
 }
 
 const JsProcessor = {
