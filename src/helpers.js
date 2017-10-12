8efec15b17e6d300ce0f2dc6896bb58cb19efcd9
Marcel Miranda <mmiranda@travix.com>
2017-10-12 17:11:50 +0200

    Add simple rgb helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,10 @@
-function hexToRgb(hexStr) {
+/**
+ * Converts hex string into it's comma separated 0-255 values
+ * Example: rgb('#FFF') => '255, 255, 255'
+ *
+ * @param {String} Hex value in the format of #FFF or #FFFFFF
+ */
+function rgb(hexStr) {
   const hex = hexStr.replace(/^#/, '');
   const fullHex = hex.length === 3
     ? `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`
@@ -6,14 +12,21 @@ function hexToRgb(hexStr) {
 
   const num = parseInt(fullHex, 16);
   // eslint-disable-next-line no-bitwise, no-mixed-operators
-  return [num >> 16, num >> 8 & 255, num & 255];
+  return [num >> 16, num >> 8 & 255, num & 255].join(', ');
 }
 
-function rgba(hexStr = '', opacity = 1) {
-  const values = hexToRgb(hexStr).concat(opacity).join(', ');
-  return `rgba(${values})`;
+/**
+ * Converts hex string and opacity pair into a proper rgba definition
+ * Example: rgba('#FFF', 0.5) => 'rgba(255, 255, 255, 0.5)'
+ *
+ * @param {String} Hex value in the format of #FFF or #FFFFFF
+ * @param {Number} Opacity from 0 to 1
+ */
+function rgba(hexStr, opacity = 1) {
+  return `rgba(${rgb(hexStr)}, ${opacity})`;
 }
 
 module.exports = {
+  rgb,
   rgba,
 };
--- tests/specs/helpers.spec.js
+++ tests/specs/helpers.spec.js
@@ -1,23 +1,27 @@
 import helpers from '../../src/helpers';
 
-describe('Helpers: rgba', () => {
-  it('should convert a hex color value into rgba', () => {
-    expect(helpers.rgba('#FFFFFF')).toEqual('rgba(255, 255, 255, 1)');
+describe('Helpers', () => {
+  describe('rgb', () => {
+    it('should convert a hex color value into rgba', () => {
+      expect(helpers.rgb('#FFFFFF')).toEqual('255, 255, 255');
+    });
+
+    it('should handle shorthand hex color definitions correctly', () => {
+      expect(helpers.rgb('#F00')).toEqual('255, 0, 0');
+    });
+
+    it('should handle other kinds of colors', () => {
+      expect(helpers.rgb('#99424f')).toEqual('153, 66, 79');
+    });
   });
 
-  it('should handle shorthand hex color definitions correctly', () => {
-    expect(helpers.rgba('#F00')).toEqual('rgba(255, 0, 0, 1)');
-  });
+  describe('rgba', () => {
+    it('should convert a hex color value into rgba', () => {
+      expect(helpers.rgba('#FFFFFF')).toEqual('rgba(255, 255, 255, 1)');
+    });
 
-  it('should append use passed opacity', () => {
-    expect(helpers.rgba('#00FF00', 0.5)).toEqual('rgba(0, 255, 0, 0.5)');
-  });
-
-  it('should handle other kinds of colors', () => {
-    expect(helpers.rgba('#99424f', 1)).toEqual('rgba(153, 66, 79, 1)');
-  });
-
-  it('Handles undefined color as black', () => {
-    expect(helpers.rgba(undefined, 1)).toEqual('rgba(0, 0, 0, 1)');
+    it('should append use passed opacity', () => {
+      expect(helpers.rgba('#00FF00', 0.5)).toEqual('rgba(0, 255, 0, 0.5)');
+    });
   });
 });
